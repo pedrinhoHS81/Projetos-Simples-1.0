fd79646fa9f6b1b012088e01aea1641268f7057f
PedroHenriqueSA <pedrohenri8599@gmail.com>
2026-06-09 09:21:11 -0300

    Delete Minha Primeira Pagina Web directory

--- Minha Primeira Pagina Web/Script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById("botaoMenssagem");
-const mensagem = document.getElementById("menssagem");
-
-const texto = "Voce criou seu primeiro Front-End, parabens!";
-
-botao.addEventListener("click", () => {
-    mensagem.textContent = texto;
-});
